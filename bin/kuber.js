1ab8a3fd077fc9419f15c889af20215d1aff3c65
Tiago Luchini <luchini@work.co>
2014-11-28 13:42:37 -0200

    Initial boot2docker checks

--- bin/kuber.js
+++ bin/kuber.js
@@ -2,13 +2,14 @@
 
 var nopt = require('nopt'),
     config = require('../lib/config'),
+    systemcheck = require('../lib/cli/system-check'),
     tree = require('../lib/tree').parsed();
-
-
 
 var parsed = nopt(config.types, config.shorthands);
 
 console.log(parsed);
+
+systemcheck.run();
 
 var cmd = parsed.argv.cooked.shift();
 
